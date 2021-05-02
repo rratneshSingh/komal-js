98d08793d6cf161233ae0f6eb81e8c81aa8627d1
ratnesh singh <dev.ratnesh.singh@gmail.com>
2021-05-02 21:10:59 +0530

    array

---
+++ array.js
@@ -0,0 +1,17 @@
+var arrLiteral = [ 'Ram', 'Shyam', 'Ramesh', 'Suresh', 'Geeta' ]; // array literal - []
+var arrObject = new Array( 1, 2, 3, 4, 5 ); // arr object
+
+var str = arrLiteral.toString();
+// console.log( Array.prototype.toString );
+console.log( str );
+
+// console.log( Object.prototype.toString.call( arrLiteral) );
+
+var newArr = [ 1,2,3, ];
+
+var p = arrLiteral.slice( 1, 4 );
+console.log( arrLiteral );
+console.log( p );
+
+
+
